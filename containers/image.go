6f5f180a4db9e93f3712b1c71286aa375696d657
Cyril Connan <cyril.connan@airbus.com>
2021-10-19 13:25:53 +0000

    Update dest mangement and remove fetchdigest func

--- containers/image.go
+++ containers/image.go
@@ -15,14 +15,13 @@ import (
 	"github.com/containers/image/v5/signature"
 	"github.com/containers/image/v5/transports/alltransports"
 	"github.com/containers/image/v5/types"
-	"github.com/opencontainers/go-digest"
 )
 
 // DownloadImage download docker images from src and create docker-archive
-func DownloadImage(imgList []string, dest string, fetchSignature bool) error {
+func DownloadImage(imgList []string, destImg string, fetchSignature bool, destSig string) error {
 
 	// Create new dest archive
-	aw, err := archive.NewWriter(nil, fmt.Sprintf("%s/images.tar", dest))
+	aw, err := archive.NewWriter(nil, destImg)
 	if err != nil {
 		log.Printf("Error when initializing destination archive.\n")
 		return err
@@ -34,7 +33,6 @@ func DownloadImage(imgList []string, dest string, fetchSignature bool) error {
 		// Ref
 		imgRef := fmt.Sprintf("%s%s", "docker://", img)
 		srcRef, err := alltransports.ParseImageName(imgRef)
-
 		if err != nil {
 			log.Printf("Error when parsing image name for %s", img)
 			return err
@@ -59,8 +57,8 @@ func DownloadImage(imgList []string, dest string, fetchSignature bool) error {
 		}
 
 		// Download and create tar
-		fmt.Printf("Copy %s to %s\n", img, dest)
-		err = copyImg(context.Background(), srcRef, destRef)
+		fmt.Printf("Copy %s to %s\n", img, destImg)
+		rawManifest, err := copyImg(context.Background(), srcRef, destRef)
 		if err != nil {
 			log.Printf("Error when downloading image %s", img)
 			return err
@@ -69,9 +67,9 @@ func DownloadImage(imgList []string, dest string, fetchSignature bool) error {
 		// Fetching Cosing signature
 		// Do not fail at signature error (the signature may not exist yet)
 		if fetchSignature {
-			imgDigest, err := fetchDigest(srcRef)
+			imgDigest, err := manifest.Digest(rawManifest)
 			if err != nil {
-				log.Printf("Error when fecthing digest image %s", srcRef.DockerReference().Name())
+				log.Printf("Error when fecthing digest image %s \n %s", srcRef.DockerReference().Name(), err)
 				continue
 			}
 
@@ -79,22 +77,24 @@ func DownloadImage(imgList []string, dest string, fetchSignature bool) error {
 			imgRefSig := fmt.Sprintf("%s%s:%s%s", "docker://", strings.Split(imgNameTagged.Name(), ":")[0], strings.Replace(imgDigest.String(), ":", "-", 1), ".sig")
 			srcRefSig, err := alltransports.ParseImageName(imgRefSig)
 			if err != nil {
-				log.Printf("Error when downloading signature %s", imgRefSig)
+				log.Printf("Error when downloading signature %s \n %s", imgRefSig, err)
 				continue
 			}
 			// Creating directory for signature
-			destDirSig := fmt.Sprintf("%s/%s/%s%s", dest, "signature", strings.Replace(imgDigest.String(), ":", "-", 1), ".sig")
+			// destDirSig := fmt.Sprintf("%s/%s/%s%s", dest, "signature", strings.Replace(imgDigest.String(), ":", "-", 1), ".sig")
+			destDirSig := fmt.Sprintf("%s/%s%s", destSig, strings.Replace(imgDigest.String(), ":", "-", 1), ".sig")
 			helpers.CreateDestDir(destDirSig)
 			path, _ := os.Getwd()
+
 			// Creating destination reference (Since it's not tar layered cannot reuse same method as above)
 			destRefSig, err := alltransports.ParseImageName(fmt.Sprintf("%s/%s/%s", "dir:", path, destDirSig))
 			if err != nil {
-				log.Printf("Error when downloading signature %s", imgRefSig)
+				log.Printf("Error when downloading signature %s \n %s", imgRefSig, err)
 				continue
 			}
-			err = copyImg(context.Background(), srcRefSig, destRefSig)
+			_, err = copyImg(context.Background(), srcRefSig, destRefSig)
 			if err != nil {
-				log.Printf("Error when downloading signature %s", imgRefSig)
+				log.Printf("Error when downloading signature %s \n %s", imgRefSig, err)
 				continue
 			}
 		}
@@ -102,17 +102,17 @@ func DownloadImage(imgList []string, dest string, fetchSignature bool) error {
 	return nil
 }
 
-func copyImg(ctx context.Context, srcImgRef types.ImageReference, destImgRef types.ImageReference) error {
+func copyImg(ctx context.Context, srcImgRef types.ImageReference, destImgRef types.ImageReference) (manifest []byte, err error) {
 	// Contexts
 	defaultPolicy, err := signature.NewPolicyFromFile("policy.json")
 	if err != nil {
 		log.Printf("Default policy err.\n")
-		return err
+		return nil, err
 	}
 	policyContext, err := signature.NewPolicyContext(defaultPolicy)
 	if err != nil {
 		log.Printf("Policy context err.\n")
-		return err
+		return nil, err
 	}
 	defer policyContext.Destroy()
 	// Create systemContext to select os and arch
@@ -120,32 +120,10 @@ func copyImg(ctx context.Context, srcImgRef types.ImageReference, destImgRef typ
 		ArchitectureChoice: "amd64",
 		OSChoice:           "linux",
 	}
-	_, err = copy.Image(context.Background(), policyContext, destImgRef, srcImgRef, &copy.Options{
+	return copy.Image(context.Background(), policyContext, destImgRef, srcImgRef, &copy.Options{
 		ReportWriter: os.Stdout,
 		SourceCtx:    sysCtx,
 	})
-	return err
-}
-
-func fetchDigest(srcImgRef types.ImageReference) (*digest.Digest, error) {
-	// Create systemContext to select os and arch
-	sysCtx := &types.SystemContext{
-		ArchitectureChoice: "amd64",
-		OSChoice:           "linux",
-	}
-	imgSrc, err := srcImgRef.NewImageSource(context.Background(), sysCtx)
-	if err != nil {
-		return nil, err
-	}
-	rawManifest, _, err := imgSrc.GetManifest(context.Background(), nil)
-	if err != nil {
-		return nil, err
-	}
-	digest, err := manifest.Digest(rawManifest)
-	if err != nil {
-		return nil, err
-	}
-	return &digest, err
 }
 
 func getImgTag(imgStr string) string {
